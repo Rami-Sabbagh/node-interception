8e9d514335c019066bfa00f057a21d0050f715be
Rami Sabbagh <ramilego4game@gmail.com>
2021-08-09 23:04:22 +0300

    Update `index.ts` to include re-exports instead of test code

--- lib/index.ts
+++ lib/index.ts
@@ -1,30 +1,13 @@
-import * as os from 'os';
+export {
+    FilterKeyState, FilterMouseState, MouseFlag,
+    KeyState, MouseState,
+    MouseStroke, KeyboardStroke, InvalidStroke, Stroke,
+    MAX_DEVICE, MAX_KEYBOARD, MAX_MOUSE
+} from './addon';
 
-import native, { FilterKeyState, FilterMouseState } from './addon';
+export { default as Device, Mouse, Keyboard, InvalidDevice } from './device';
 
-os.setPriority(os.constants.priority.PRIORITY_HIGH);
+export { default as Interception } from './interception';
 
-const SCANCODE_ESC = 0x01;
-
-const context = native.createContext();
-
-native.setFilter(context, 'mouse', FilterMouseState.MOVE);
-native.setFilter(context, 'keyboard', FilterKeyState.DOWN);
-
-while (true) {
-    const device = native.wait(context);
-    if (device === null) break;
-    const stroke = native.receive(context, device, 1);
-    if (stroke === null) break;
-
-    if (stroke.type === 'keyboard' && stroke.code == SCANCODE_ESC) break;
-    if (stroke.type === 'invalid') continue;
-
-    native.send(context, device, stroke, 1);
-    console.log('Device:', device, 'Stroke: ', stroke);
-}
-
-native.setFilter(context, 'mouse', FilterMouseState.NONE);
-native.setFilter(context, 'keyboard', FilterKeyState.NONE);
-
-native.destroyContext(context);
+import Interception from './interception';
+export default Interception;

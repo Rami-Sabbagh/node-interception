b93d5ad0700ca96335148423a32f51d7f7a4dcfb
Rami Sabbagh <ramilego4game@gmail.com>
2021-08-09 15:47:50 +0300

    Prefix some types names with `I` to distinguish them

--- lib/addon.ts
+++ lib/addon.ts
@@ -1,6 +1,6 @@
-export type Context = External;
-export type Device = number;
-export type Filter = number;
+export type IContext = External;
+export type IDevice = number;
+export type IFilter = number;
 
 export const MAX_KEYBOARD = 10;
 export const MAX_MOUSE = 10;
@@ -10,12 +10,12 @@ export const MAX_DEVICE = MAX_KEYBOARD + MAX_MOUSE;
  * Gets the device ID of the `i`th keyboard.
  * @param index The number of the keyboard, 0-based, maximum: `MAX_KEYBOARD - 1`.
  */
-export const KEYBOARD = (index: number): Device => index + 1;
+export const KEYBOARD = (index: number): IDevice => index + 1;
 /**
  * Gets the device ID of the `i`th mouse.
  * @param index The number of the mouse, 0-based, maximum: `MAX_MOUSE - 1`.
  */
-export const MOUSE = (index: number): Device => MAX_KEYBOARD + index + 1;
+export const MOUSE = (index: number): IDevice => MAX_KEYBOARD + index + 1;
 
 export enum KeyState {
     DOWN = 0x00,
@@ -128,82 +128,82 @@ export interface InterceptionNative {
     /**
      * Creates an interception context, inorder for executing any api operation.
      */
-    createContext(): Context;
+    createContext(): IContext;
     /**
      * Destroys an interception context, automatically handled when a context is garbage collected.
      * Any further api calls using this context would instantly fail or result with unknown behaviour.
      */
-    destroyContext(context: Context): void;
+    destroyContext(context: IContext): void;
     /**
      * Checks whether if a context is already destoyed or not.
      */
-    isContextDestroyed(context: Context): boolean;
+    isContextDestroyed(context: IContext): boolean;
 
     /**
      * Gets the precedence (priority) which this context has for that specific device.
      */
-    getPrecedence(context: Context, device: Device): number;
+    getPrecedence(context: IContext, device: IDevice): number;
     /**
      * Sets the precedence (priority) which this context wants for that specific device.
      */
-    setPrecedence(context: Context, device: Device, precendence: number): void;
+    setPrecedence(context: IContext, device: IDevice, precendence: number): void;
 
-    getFilter(context: Context, device: Device): Filter;
+    getFilter(context: IContext, device: IDevice): IFilter;
     /**
      * Sets the stroke events that get intercepted by this instance of the library.
      * Each predicate has it's own filters state, initially set to 0 (NONE).
      */
-    setFilter(context: Context, predicate: 'keyboard' | 'mouse' | 'invalid', filter: Filter): void;
+    setFilter(context: IContext, predicate: 'keyboard' | 'mouse' | 'invalid', filter: IFilter): void;
 
     /**
      * Waits for a device to send a stroke, or null on failure.
      */
-    wait(context: Context): Device | null;
+    wait(context: IContext): IDevice | null;
     /**
      * Waits asynchronously for device to send a stroke, or null on failure.
      * Any call to any wait method would fail until the promise is resolved either with success or failure.
      */
-    waitAsync(context: Context): Promise<Device | null>;
+    waitAsync(context: IContext): Promise<IDevice | null>;
 
     /**
      * Waits for a device to send a stroke with a timeout, results with null on failure or timeout.
      * @param timeout in milliseconds.
      */
-    waitWithTimeout(context: Context, timeout: number): Device | null;
+    waitWithTimeout(context: IContext, timeout: number): IDevice | null;
     /**
      * Waits asynchronously for a device to send a stroke with a timeout, results with null on failure or timeout.
      * @param timeout in milliseconds.
      */
-    waitWithTimeoutAsync(context: Context, timeout: number): Promise<Device | null>;
+    waitWithTimeoutAsync(context: IContext, timeout: number): Promise<IDevice | null>;
 
     /**
      * Sends a stroke under a specific device.
      */
-    send(context: Context, device: Device, stroke: KeyboardStroke | MouseStroke, nstroke: number): boolean;
+    send(context: IContext, device: IDevice, stroke: KeyboardStroke | MouseStroke, nstroke: number): boolean;
     /**
      * Receives the stroke sent by a specific device, after waiting for it using one of the wait methods.
      * @returns null on failure.
      */
-    receive(context: Context, device: Device, nstroke: number): Stroke | null;
+    receive(context: IContext, device: IDevice, nstroke: number): Stroke | null;
 
     /**
      * Gets the hardware id of a specific device, which may help on disambiguation of device input.
      * The hardware ids are not requried to be unique, but mostly will when you have at least two different device models.
      */
-    getHardwareId(context: Context, device: Device): string | null;
+    getHardwareId(context: IContext, device: IDevice): string | null;
 
     /**
      * Checks whether a device id is invalid or not.
      */
-    isInvalid(device: Device): boolean;
+    isInvalid(device: IDevice): boolean;
     /**
      * Checks whether a device id is for a keyboard device or not.
      */
-    isKeyboard(device: Device): boolean;
+    isKeyboard(device: IDevice): boolean;
     /**
      * Checks whether a device id is for a mouse device or not.
      */
-    isMouse(device: Device): boolean;
+    isMouse(device: IDevice): boolean;
 }
 
 const native: InterceptionNative = require('../build/Release/node_interception.node');

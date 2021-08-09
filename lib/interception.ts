621a7c936f2c7f99875a0b7ec5ccb3d5482f7be8
Rami Sabbagh <ramilego4game@gmail.com>
2021-08-09 23:02:01 +0300

    Create a wrapper for the main context methods

---
+++ lib/interception.ts
@@ -0,0 +1,85 @@
+import interception, { IDevice, IFilter, MAX_DEVICE, MAX_KEYBOARD, MAX_MOUSE } from './addon';
+import Device, { Keyboard, Mouse } from './device';
+
+export default class Interception {
+    constructor(private readonly _context = interception.createContext()) { }
+
+    get context() {
+        if (interception.isContextDestroyed(this._context)) throw new Error('The context is destroyed');
+        return this._context;
+    }
+
+    setFilter(type: 'keyboard' | 'mouse', filter: IFilter): void {
+        interception.setFilter(this.context, type, filter);
+    }
+
+    getDevice(id: IDevice): Device {
+        return new Device(this._context, id);
+    }
+
+    getDevices(): Device[] {
+        const devices: Device[] = [];
+
+        const context = this.context; // So the destruction check is only done once.
+
+        for (let id = 1; id <= MAX_DEVICE; id++) {
+            if (interception.getHardwareId(context, id) === null) continue;
+            devices.push(new Device(context, id));
+        }
+
+        return devices;
+    }
+
+    getKeyboards(): Keyboard[] {
+        const keyboards: Keyboard[] = [];
+
+        const context = this.context; // So the destruction check is only done once.
+
+        for (let id = 1; id <= MAX_KEYBOARD; id++) {
+            if (interception.getHardwareId(context, id) === null) continue;
+            keyboards.push(new Device(context, id));
+        }
+
+        return keyboards;
+    }
+
+    getMice(): Mouse[] {
+        const mice: Mouse[] = [];
+
+        const context = this.context; // So the destruction check is only done once.
+
+        for (let id = MAX_KEYBOARD + 1; id <= MAX_KEYBOARD + MAX_MOUSE; id++) {
+            if (interception.getHardwareId(context, id) === null) continue;
+            mice.push(new Device(context, id));
+        }
+
+        return mice;
+    }
+
+    async wait(timeout?: number): Promise<Device | null> {
+        let id = null;
+
+        if (timeout === undefined) id = await interception.waitAsync(this.context);
+        else id = await interception.waitWithTimeoutAsync(this.context, timeout);
+
+        return id !== null ? new Device(this._context, id) : null;
+    }
+
+    waitSync(timeout?: number): Device | null {
+        let id = null;
+
+        if (timeout === undefined) id = interception.wait(this.context);
+        else id = interception.waitWithTimeout(this.context, timeout);
+
+        return id !== null ? new Device(this._context, id) : null;
+    }
+
+    destroy(): void {
+        if (interception.isContextDestroyed(this._context)) throw new Error('Already destroyed');
+        interception.destroyContext(this._context);
+    }
+
+    isDestroyed(): boolean {
+        return interception.isContextDestroyed(this._context);
+    }
+}

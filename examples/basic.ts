f1af97eaf500fad504f0d1365210c24a5c430ece
Rami Sabbagh <ramilego4game@gmail.com>
2021-08-11 05:30:01 +0300

    Minor improvement

--- examples/basic.ts
+++ examples/basic.ts
@@ -11,12 +11,14 @@ console.log('Devices:', interception.getDevices().map(device => `${device}`));
 interception.setFilter('keyboard', FilterKeyState.ALL);
 interception.setFilter('mouse', FilterMouseState.ALL);
 
+const SCANCODE_ESC = 0x01;
+
 async function listen() {
     while (true) {
         const device = await interception.wait();
         const stroke = device?.receive();
 
-        if (!device || !stroke || (stroke?.type === 'keyboard' && stroke.code === 0x01)) break;
+        if (!device || !stroke || (stroke?.type === 'keyboard' && stroke.code === SCANCODE_ESC)) break;
 
         console.log(device.toString(), stroke);
     }
